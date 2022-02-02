a9e7cdab573595f4d7adfd45513c099810ac2f4e
Faysal Jamil Nadim <faysaljamil@Faysals-MacBook-Pro.local>
2022-02-02 21:50:01 +0600

    s3

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,32 +1,67 @@
-const express = require('express');
-const { reqSubmit, getRequestP, getRequestA, getRequestR, updateRequest, rejectRequest, userRequestA, userRequestR, userRequestP } = require('../controller/request');
-const { requireSignIn, userMiddleware, adminMiddleware } = require('../middleware');
+const express = require("express");
+const {
+  reqSubmit,
+  getRequestP,
+  getRequestA,
+  getRequestR,
+  updateRequest,
+  rejectRequest,
+  userRequestA,
+  userRequestR,
+  userRequestP,
+} = require("../controller/request");
+const {
+  requireSignIn,
+  userMiddleware,
+  adminMiddleware,
+  uploads3,
+} = require("../middleware");
 const router = express.Router();
-const multer = require('multer');
-const shortid = require('shortid');
-const path = require('path');
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.join(path.dirname(__dirname), 'uploads'))
-    },
-    filename: function (req, file, cb) {
-      cb(null, shortid.generate() + '-' + file.originalname)
-    }
-  });
-  
-  const upload = multer({ storage });
-
-router.post('/request', requireSignIn, userMiddleware, reqSubmit);
-router.get('/get/request/pending', requireSignIn, adminMiddleware, getRequestP);
-router.get('/get/request/approved', requireSignIn, adminMiddleware, getRequestA);
-router.get('/get/request/rejected', requireSignIn, adminMiddleware, getRequestR);
-router.post('/request/update', requireSignIn, adminMiddleware, upload.array('productImage'), updateRequest);
-router.post('/request/update/reject', requireSignIn, adminMiddleware, rejectRequest);
-router.get('/user/requests/approved', requireSignIn, userMiddleware, userRequestA);
-router.get('/user/requests/pending', requireSignIn, userMiddleware, userRequestP);
-router.get('/user/requests/rejected', requireSignIn, userMiddleware, userRequestR);
-
-
+router.post("/request", requireSignIn, userMiddleware, reqSubmit);
+router.get("/get/request/pending", requireSignIn, adminMiddleware, getRequestP);
+router.get(
+  "/get/request/approved",
+  requireSignIn,
+  adminMiddleware,
+  getRequestA
+);
+router.get(
+  "/get/request/rejected",
+  requireSignIn,
+  adminMiddleware,
+  getRequestR
+);
+router.post(
+  "/request/update",
+  requireSignIn,
+  adminMiddleware,
+  uploads3.array("productImage"),
+  updateRequest
+);
+router.post(
+  "/request/update/reject",
+  requireSignIn,
+  adminMiddleware,
+  rejectRequest
+);
+router.get(
+  "/user/requests/approved",
+  requireSignIn,
+  userMiddleware,
+  userRequestA
+);
+router.get(
+  "/user/requests/pending",
+  requireSignIn,
+  userMiddleware,
+  userRequestP
+);
+router.get(
+  "/user/requests/rejected",
+  requireSignIn,
+  userMiddleware,
+  userRequestR
+);
 
 module.exports = router;
